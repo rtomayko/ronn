15a10f1a4a079be869174f2779d4e794d8227b7b
Ryan Tomayko <rtomayko@gmail.com>
2010-06-14 23:46:04 -0700

    meta generator links to that ronn version source tree

--- lib/ronn/template.rb
+++ lib/ronn/template.rb
@@ -55,7 +55,7 @@ module Ronn
     end
 
     def generator
-      "Ronn/v#{Ronn::VERSION} (http://github.com/rtomayko/ronn)"
+      "Ronn/v#{Ronn.version} (http://github.com/rtomayko/ronn/tree/#{Ronn.revision})"
     end
 
     def manual
